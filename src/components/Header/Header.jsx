2c1afa6517d807897adae38de42f15b33f0f86e6
lucifer098a <nk5141030@gmail.com>
2022-08-23 01:03:55 +0530

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import Search from "../Search";
+import { AppBar, Toolbar, Box, Typography, styled } from "@mui/material";
+
+const StyledNavbar = styled(AppBar)`
+  background: #2874f0;
+  height: 55px;
+`;
+
+const Component = styled(Box)`
+  margin-left: 12%;
+  line-height: 0;
+`;
+
+const SubHeading = styled(Typography)`
+  font-size: 10px;
+  font-style: italic;
+`;
+
+const PlusImage = styled("img")({
+  width: 10,
+  height: 10,
+  marginLeft: 4,
+});
 
 const Header = () => {
-  return <div>Header</div>;
+  const logoURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+  const subURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
+
+  return (
+    <StyledNavbar>
+      <Toolbar>
+        <Component>
+          <img src={logoURL} alt="logo" style={{ width: 75 }} />
+          <Box style={{ display: "flex" }}>
+            <SubHeading>
+              Explore&nbsp;
+              <Box component="span" style={{ color: "#FFE500" }}>
+                Plus
+              </Box>
+            </SubHeading>
+            <PlusImage src={subURL} alt="plus" />
+          </Box>
+        </Component>
+        <Box>
+          <Search />
+        </Box>
+      </Toolbar>
+    </StyledNavbar>
+  );
 };
 
 export default Header;
